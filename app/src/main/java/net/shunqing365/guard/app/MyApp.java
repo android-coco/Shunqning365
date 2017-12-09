03cf6a98b111676b86ba1a3cae8caa6b1c7fb4d3
yhlyl <yh_android@163.com>
2017-12-09 22:50:01 +0800

    Initial commit

---
+++ app/src/main/java/net/shunqing365/guard/app/MyApp.java
@@ -0,0 +1,16 @@
+package net.shunqing365.guard.app;
+
+import yh.org.shunqinglib.app.ShunQingApp;
+
+/**
+ * Created by yhlyl on 2017/12/9.
+ */
+
+public class MyApp extends ShunQingApp
+{
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+}
