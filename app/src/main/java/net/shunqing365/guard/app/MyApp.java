324955dfac548d6af4c4b061a074a9ba2bd5c0b3
yhlyl <yh_android@163.com>
2017-12-11 00:02:11 +0800

    友盟分享

--- app/src/main/java/net/shunqing365/guard/app/MyApp.java
+++ app/src/main/java/net/shunqing365/guard/app/MyApp.java
@@ -1,5 +1,10 @@
 package net.shunqing365.guard.app;
 
+import com.umeng.socialize.Config;
+import com.umeng.socialize.PlatformConfig;
+import com.umeng.socialize.UMShareAPI;
+import com.umeng.socialize.common.QueuedWork;
+
 import yh.org.shunqinglib.app.ShunQingApp;
 
 /**
@@ -12,5 +17,18 @@ public class MyApp extends ShunQingApp
     public void onCreate()
     {
         super.onCreate();
+        PlatformConfig.setWeixin("wx2c2cbc1f19585fbf","be0c1c3e86fe7298748fa46087d73cb8");
+        PlatformConfig.setQQZone("101057756","294c113931c7a863460ccbbc9f3bf0c5");
+        //开启debug模式，方便定位错误，具体错误检查方式可以查看http://dev.umeng.com/social/android/quick-integration的报错必看，正式发布，请关闭该模式
+        Config.DEBUG = true;
+        QueuedWork.isUseThreadPool = false;
+        UMShareAPI.get(this);
+    }
+
+    //各个平台的配置，建议放在全局Application或者程序入口
+   {
+        PlatformConfig.setWeixin("wxdc1e388c3822c80b", "3baf1193c85774b3fd9d18447d76cab0");
+        //豆瓣RENREN平台目前只能在服务器端配置
+        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
     }
 }
---
+++ app/src/main/java/net/shunqing365/guard/wxapi/WXEntryActivity.java
@@ -0,0 +1,14 @@
+package net.shunqing365.guard.wxapi;
+
+
+
+//import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+public class WXEntryActivity extends WXCallbackActivity
+{
+
+
+
+}
