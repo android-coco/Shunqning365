ebc88e761028906fb59ca5522786a6336c057047
youhao <yh_android@163.com>
2017-12-14 11:36:35 +0800

    接口加入

--- app/src/main/java/net/shunqing365/guard/app/MyApp.java
+++ app/src/main/java/net/shunqing365/guard/app/MyApp.java
@@ -17,8 +17,6 @@ public class MyApp extends ShunQingApp
     public void onCreate()
     {
         super.onCreate();
-        PlatformConfig.setWeixin("wx2c2cbc1f19585fbf","be0c1c3e86fe7298748fa46087d73cb8");
-        PlatformConfig.setQQZone("101057756","294c113931c7a863460ccbbc9f3bf0c5");
         //开启debug模式，方便定位错误，具体错误检查方式可以查看http://dev.umeng.com/social/android/quick-integration的报错必看，正式发布，请关闭该模式
         Config.DEBUG = true;
         QueuedWork.isUseThreadPool = false;
@@ -27,7 +25,7 @@ public class MyApp extends ShunQingApp
 
     //各个平台的配置，建议放在全局Application或者程序入口
    {
-        PlatformConfig.setWeixin("wxdc1e388c3822c80b", "3baf1193c85774b3fd9d18447d76cab0");
+        PlatformConfig.setWeixin("wx22a40574ac7135d5", "ee5aa2bc82e047bbc04eb9844e313688");
         //豆瓣RENREN平台目前只能在服务器端配置
         PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
     }
--- app/src/main/java/net/shunqing365/guard/ui/LoginActivity.java
+++ app/src/main/java/net/shunqing365/guard/ui/LoginActivity.java
@@ -1,14 +1,26 @@
 package net.shunqing365.guard.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import com.umeng.socialize.UMAuthListener;
+import com.umeng.socialize.UMShareAPI;
+import com.umeng.socialize.UMShareConfig;
+import com.umeng.socialize.bean.SHARE_MEDIA;
 
 import net.shunqing365.guard.R;
 import net.shunqing365.guard.view.ZQImageViewRoundOval;
 
 import org.yh.library.ui.BindView;
 import org.yh.library.ui.YHViewInject;
+import org.yh.library.utils.LogUtils;
+import org.yh.library.utils.StringUtils;
+import org.yh.library.view.loading.dialog.YHLoadingDialog;
 import org.zackratos.ultimatebar.UltimateBar;
+
+import java.util.Map;
 
 import yh.org.shunqinglib.base.BaseActiciy;
 
@@ -17,11 +29,16 @@ import yh.org.shunqinglib.base.BaseActiciy;
  */
 public class LoginActivity extends BaseActiciy
 {
-
+    //    public ArrayList<SnsPlatform> platforms = new ArrayList<SnsPlatform>();
+//    private SHARE_MEDIA[] list = {SHARE_MEDIA.QQ, SHARE_MEDIA.SINA, SHARE_MEDIA.WEIXIN,
+//            SHARE_MEDIA.FACEBOOK, SHARE_MEDIA.TWITTER, SHARE_MEDIA.LINKEDIN, SHARE_MEDIA.DOUBAN, SHARE_MEDIA.RENREN, SHARE_MEDIA.KAKAO,
+//            SHARE_MEDIA.VKONTAKTE, SHARE_MEDIA.DROPBOX};
     @BindView(id = R.id.login_winxin, click = true)
     TextView login_winxin;//登录
     @BindView(id = R.id.img_bg)
     private ZQImageViewRoundOval iv_roundRect;//圆角矩形图片
+    UMShareAPI umShareAPI;
+
     @Override
     public void setRootView()
     {
@@ -29,6 +46,78 @@ public class LoginActivity extends BaseActiciy
         ultimateBar.setHintBar();
         setContentView(R.layout.activity_login);
     }
+
+    @Override
+    public void initData()
+    {
+        super.initData();
+        //initPlatforms();
+        UMShareConfig config = new UMShareConfig();
+        config.isNeedAuthOnGetUserInfo(true);
+        umShareAPI = UMShareAPI.get(aty);
+        umShareAPI.setShareConfig(config);
+    }
+
+    //    private void initPlatforms() {
+//        platforms.clear();
+//        for (SHARE_MEDIA e : list) {
+//            if (!e.toString().equals(SHARE_MEDIA.GENERIC.toString())) {
+//                platforms.add(e.toSnsPlatform());
+//            }
+//        }
+//    }
+
+    UMAuthListener authListener = new UMAuthListener()
+    {
+        /**
+         * @desc 授权开始的回调
+         * @param platform 平台名称
+         */
+        @Override
+        public void onStart(SHARE_MEDIA platform)
+        {
+            YHLoadingDialog.make(aty).show();
+        }
+
+        /**
+         * @desc 授权成功的回调
+         * @param platform 平台名称
+         * @param action 行为序号，开发者用不上
+         * @param data 用户资料返回
+         */
+        @Override
+        public void onComplete(SHARE_MEDIA platform, int action, Map<String, String> data)
+        {
+            YHLoadingDialog.make(aty).cancelDialog();
+            YHViewInject.create().showTips("登录成功");
+            LogUtils.e(TAG, data);
+        }
+
+        /**
+         * @desc 授权失败的回调
+         * @param platform 平台名称
+         * @param action 行为序号，开发者用不上
+         * @param t 错误原因
+         */
+        @Override
+        public void onError(SHARE_MEDIA platform, int action, Throwable t)
+        {
+            YHViewInject.create().showTips("授权失败：" + t.getMessage());
+            YHLoadingDialog.make(aty).cancelDialog();
+        }
+
+        /**
+         * @desc 授权取消的回调
+         * @param platform 平台名称
+         * @param action 行为序号，开发者用不上
+         */
+        @Override
+        public void onCancel(SHARE_MEDIA platform, int action)
+        {
+            YHViewInject.create().showTips("取消登录");
+            YHLoadingDialog.make(aty).cancelDialog();
+        }
+    };
 
     @Override
     public void initWidget()
@@ -45,8 +134,46 @@ public class LoginActivity extends BaseActiciy
         switch (v.getId())
         {
             case R.id.login_winxin:
+                final boolean isauth = umShareAPI.isAuthorize(aty, SHARE_MEDIA.WEIXIN);
                 YHViewInject.create().showTips("登录");
+                if (isauth)
+                {
+                    umShareAPI.deleteOauth(aty, SHARE_MEDIA.WEIXIN, authListener);
+                }
+                umShareAPI.doOauthVerify(this, SHARE_MEDIA.WEIXIN, authListener);
+                umShareAPI.getPlatformInfo(aty, SHARE_MEDIA.WEIXIN, authListener);
                 break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (!StringUtils.isEmpty(umShareAPI))
+        {
+            umShareAPI.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        if (!StringUtils.isEmpty(umShareAPI))
+        {
+            umShareAPI.release();
+        }
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        if (!StringUtils.isEmpty(umShareAPI))
+        {
+            umShareAPI.onSaveInstanceState(outState);
         }
     }
 }
