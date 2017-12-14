2dc9ac7b336705b4775411d1040d10d04145092d
youhao <yh_android@163.com>
2017-12-14 11:36:19 +0800

    接口加入

---
+++ app/src/main/java/net/shunqing365/guard/util/AppUtil.java
@@ -0,0 +1,14 @@
+package net.shunqing365.guard.util;
+
+/**
+ * 作者：游浩 on 2017/12/14 10:04
+ * https://github.com/android-coco/YhLibraryForAndroid
+ * 邮箱：yh_android@163.com
+ */
+public class AppUtil
+{
+    //主域名
+    public static final String BASE_URL = "http://service.shunqing365.com";
+    //接口名
+    public static final String INTFACE_URL = BASE_URL + "/interface/";
+}
