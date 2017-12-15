733fec6c8c5675a70126bbc24fc0b0adaff1844b
yhlyl <yh_android@163.com>
2017-12-16 00:53:18 +0800

    设备列表

--- app/src/main/java/net/shunqing365/guard/adapter/TerminalItemDelagate.java
+++ app/src/main/java/net/shunqing365/guard/adapter/TerminalItemDelagate.java
@@ -1,5 +1,7 @@
 package net.shunqing365.guard.adapter;
 
+import android.view.View;
+
 import net.shunqing365.guard.R;
 import net.shunqing365.guard.bean.TerminalJosnBen;
 
@@ -21,12 +23,26 @@ public class TerminalItemDelagate implements I_ItemViewDelegate<TerminalJosnBen.
     @Override
     public boolean isForViewType(TerminalJosnBen.TerminalModel item, int position)
     {
-        return false;
+        return true;
     }
 
     @Override
     public void convert(YHRecyclerViewHolder holder, TerminalJosnBen.TerminalModel item, int position)
     {
-
+        int type = item.getType();//0 自己的， 1为别人授权的
+        String name = item.getName();//名称
+        String sn = item.getProductSn();//SN号
+        switch (type){
+            case 0:
+                holder.getView(R.id.img_watch).setVisibility(View.VISIBLE);
+                holder.getView(R.id.img_watch1).setVisibility(View.INVISIBLE);
+                break;
+            case 1:
+                holder.getView(R.id.img_watch1).setVisibility(View.VISIBLE);
+                holder.getView(R.id.img_watch).setVisibility(View.INVISIBLE);
+                break;
+        }
+        holder.setText(R.id.timing_name,name);
+        holder.setText(R.id.sn,sn);
     }
 }
--- app/src/main/java/net/shunqing365/guard/bean/TerminalJosnBen.java
+++ app/src/main/java/net/shunqing365/guard/bean/TerminalJosnBen.java
@@ -21,24 +21,238 @@ public class TerminalJosnBen extends YHModel
 
     public static class TerminalModel implements Serializable
     {
-        private String sn;//设备SN号
-        @SerializedName("device_type")
-        private String deviceType;//终端的型号类型
-        private String keynum;//以逗号分隔，第一个为主亲情号
-        private String keysos;//SOS求助号码
-        private String latitude;//原始经度
-        private String longitude;//原始纬度
-        private String clatitude;//纠偏后的经度 （高德）
-        private String address;//纠偏后的纬度 （高德）
-        private String battery;//
-        private String signal;
+        private String name;//终端名称
+        private int id;//数据ID
+        @SerializedName("product_sn")
+        private String productSn;//SN号
+        private int signal;//所在时间地点的信号  (百分比)
+        private int battery;//所在时间的电量  (百分比)
+        @SerializedName("locate_time")
+        private int locateTime;
+        private int face;
+        private double latitude;
+        private double longitude;
         @SerializedName("locate_type")
-        private String locateType;
+        private int locateType;
+        private double clatitude;
+        private double clongitude;
+        private String address;
         @SerializedName("flag_power")
-        private String flagPower;
-        @SerializedName(" flag_battery ")
-        private String  flagBattery ;
-        private String datetime;
+        private int flagPower;
+        @SerializedName("flag_battery")
+        private int flagBattery;
+        private int type;//0 自己的， 1为别人授权的
 
+        public String getName()
+        {
+            return name;
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public void setId(int id)
+        {
+            this.id = id;
+        }
+
+        public String getProductSn()
+        {
+            return productSn;
+        }
+
+        public void setProductSn(String productSn)
+        {
+            this.productSn = productSn;
+        }
+
+        public int getSignal()
+        {
+            return signal;
+        }
+
+        public void setSignal(int signal)
+        {
+            this.signal = signal;
+        }
+
+        public int getBattery()
+        {
+            return battery;
+        }
+
+        public void setBattery(int battery)
+        {
+            this.battery = battery;
+        }
+
+        public int getLocateTime()
+        {
+            return locateTime;
+        }
+
+        public void setLocateTime(int locateTime)
+        {
+            this.locateTime = locateTime;
+        }
+
+        public int getFace()
+        {
+            return face;
+        }
+
+        public void setFace(int face)
+        {
+            this.face = face;
+        }
+
+        public double getLatitude()
+        {
+            return latitude;
+        }
+
+        public void setLatitude(double latitude)
+        {
+            this.latitude = latitude;
+        }
+
+        public double getLongitude()
+        {
+            return longitude;
+        }
+
+        public void setLongitude(double longitude)
+        {
+            this.longitude = longitude;
+        }
+
+        public int getLocateType()
+        {
+            return locateType;
+        }
+
+        public void setLocateType(int locateType)
+        {
+            this.locateType = locateType;
+        }
+
+        public double getClatitude()
+        {
+            return clatitude;
+        }
+
+        public void setClatitude(double clatitude)
+        {
+            this.clatitude = clatitude;
+        }
+
+        public double getClongitude()
+        {
+            return clongitude;
+        }
+
+        public void setClongitude(double clongitude)
+        {
+            this.clongitude = clongitude;
+        }
+
+        public String getAddress()
+        {
+            return address;
+        }
+
+        public void setAddress(String address)
+        {
+            this.address = address;
+        }
+
+        public int getFlagPower()
+        {
+            return flagPower;
+        }
+
+        public void setFlagPower(int flagPower)
+        {
+            this.flagPower = flagPower;
+        }
+
+        public int getFlagBattery()
+        {
+            return flagBattery;
+        }
+
+        public void setFlagBattery(int flagBattery)
+        {
+            this.flagBattery = flagBattery;
+        }
+
+        public int getType()
+        {
+            return type;
+        }
+
+        public void setType(int type)
+        {
+            this.type = type;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "TerminalModel{" +
+                    "name='" + name + '\'' +
+                    ", id=" + id +
+                    ", productSn='" + productSn + '\'' +
+                    ", signal=" + signal +
+                    ", battery=" + battery +
+                    ", locateTime=" + locateTime +
+                    ", face=" + face +
+                    ", latitude=" + latitude +
+                    ", longitude=" + longitude +
+                    ", locateType=" + locateType +
+                    ", clatitude=" + clatitude +
+                    ", clongitude=" + clongitude +
+                    ", address='" + address + '\'' +
+                    ", flagPower=" + flagPower +
+                    ", flagBattery=" + flagBattery +
+                    ", type=" + type +
+                    '}';
+        }
+    }
+
+    public String getResultCode()
+    {
+        return resultCode;
+    }
+
+    public void setResultCode(String resultCode)
+    {
+        this.resultCode = resultCode;
+    }
+
+    public List<TerminalModel> getDatas()
+    {
+        return datas;
+    }
+
+    public void setDatas(List<TerminalModel> datas)
+    {
+        this.datas = datas;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "TerminalJosnBen{" +
+                "resultCode='" + resultCode + '\'' +
+                ", datas=" + datas +
+                '}';
     }
 }
